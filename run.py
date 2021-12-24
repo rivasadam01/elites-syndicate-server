afe1564e9e4824ad3dd7d387f6238da91a07a14e
rivasadam01 <rivasadam01@gmail.com>
2021-12-23 21:24:30 -0800

    refactored code.

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-from flask import Flask
-from flask_cors import CORS
-from os import path
-
-basedir = path.abspath(path.dirname(__file__))
-cert = path.join(basedir, 'cert')
-
-app = Flask(__name__)
-CORS(app)
+from elites import app, path, cert
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6660, ssl_context=(
