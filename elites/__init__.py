7bcd827735fc4a712930bdc17006c00ecc56de3a
rivasadam01 <rivasadam01@gmail.com>
2021-12-23 21:24:44 -0800

    refactored code

---
+++ elites/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+from os import path
+
+basedir = path.abspath(path.dirname(__file__))
+cert = path.join(basedir, 'cert')
+
+app = Flask(__name__)
+CORS(app)
