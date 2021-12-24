117ddfaeafd5a08e7491430af88fe50ede422aa3
rivasadam01 <rivasadam01@gmail.com>
2021-12-23 21:33:34 -0800

    fixed cert path.

--- elites/__init__.py
+++ elites/__init__.py
@@ -3,7 +3,9 @@ from flask_cors import CORS
 from os import path
 
 basedir = path.abspath(path.dirname(__file__))
-cert = path.join(basedir, 'cert')
+cert = path.join(basedir, '../cert')
 
 app = Flask(__name__)
 CORS(app)
+
+import elites.routes
